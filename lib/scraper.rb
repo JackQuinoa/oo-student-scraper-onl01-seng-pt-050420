39ca94df41c614d0cada4ef64ed6b0c50dc0d824
Jack Quinoa <jquinoa208@gmail.com>
2020-07-18 16:54:50 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,17 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    index_page = Nokogiri::HTML(open(index_url))
+    students = []
+    index_page.css("div.roster-cards-container").each do |card|
+      student_url = card.css(".student-card").text
+      card.css(".card-text-container").each do |container|
+        student_name = container.css(".student-name").text
+        student_location = container.css(".student-location").text
+        students << {name: student_name, location: student_location, profile_url: student_url}
+      end
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
@@ -12,4 +22,5 @@ class Scraper
   end
 
 end
+ 
 
